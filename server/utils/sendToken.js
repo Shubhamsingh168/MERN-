dc5f991bc23976a241ab3c6770fc22323633229f
shubham <shubhamsingh07854@gmail.com>
2025-07-04 21:09:30 +0530

    add

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,11 +1,9 @@
-export const sendToken = async (user, statusCode, message, res) => {
-    const token = await user.generateToken();
+export const sendToken = async(user, statusCode, message, res) => {
+    const token = await user.generateToken(); 
     console.log(token);
     res.status(statusCode).cookie("token", token, {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",  // only true in production
-        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",  // "None" for cross-site cookie
     }).json({
         success: true,
         message,
